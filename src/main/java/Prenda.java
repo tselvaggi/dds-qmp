2c59a12db28f7023faa186eed7abe851b9f66396
tselvaggi <selvaggitomas@gmail.com>
2022-05-17 21:30:24 -0300

    QMP Primera Iteracion

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 public class Prenda {
 
   private TipoPrenda tipoPrenda;
+
   private String tipoTela;
   private Color colorPrincipal;
   private Color colorSecundario;
@@ -14,7 +15,24 @@ public class Prenda {
     this.colorPrincipal = colorPrincipal;
     this.colorSecundario = colorSecundario;
   }
+
   public boolean esValida() {
     return this.tipoPrenda != null && this.tipoTela != null && this.colorPrincipal != null;
+  }
+
+  public TipoPrenda getTipoPrenda() {
+    return tipoPrenda;
+  }
+
+  public CategoriaPrenda getCategoriaPrenda() {
+    return tipoPrenda.getCategoriaPrenda();
+  }
+
+  public String getTipoTela() {
+    return tipoTela;
+  }
+
+  public Color getColorSecundario() {
+    return colorSecundario;
   }
 }
